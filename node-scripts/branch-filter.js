b69a1bd448648cd0fb06d7f1a642d9d667b0f19a
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2026-06-27 00:43:34 -0500

    Initial commit

---
+++ node-scripts/branch-filter.js
@@ -0,0 +1,30 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import lodash from "lodash";
+
+(function start() {
+  try {
+    const issuePrefix = process.env?.ISSUE_PREFIX;
+    const safeIssuePrefix = lodash.escapeRegExp(issuePrefix);
+    let ref = process.env?.BRANCH_NAME;
+    if (!ref) {
+      ref = github.context.ref;
+    }
+    const branchName = ref.substring(ref.lastIndexOf("/") + 1);
+    console.log(`branchName = ${branchName}`);
+    core.setOutput("branchName", branchName);
+    const re = new RegExp("^" + safeIssuePrefix + "(\\d+)");
+    const prefixMatches = branchName.match(re);
+    let matches = "false";
+    let issueNumber = "";
+    if (prefixMatches) {
+      matches = "true";
+      issueNumber = prefixMatches[1];
+    }
+    core.setOutput("matches", matches);
+    core.setOutput("issueNumber", issueNumber);
+    console.log(`matches = ${matches}; issueNumber = ${issueNumber}`);
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+})();
